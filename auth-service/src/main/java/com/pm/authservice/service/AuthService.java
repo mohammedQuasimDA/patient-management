5d4fee9758894a195f5352fc58bfc8f00bb376e1
mohammedQuasimDA <mohammedquasimd@gmail.com>
2025-05-01 22:04:38 +0530

    completed analytics-service

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -30,5 +30,12 @@ public class AuthService {
         return token;
     }
 
-
+    public boolean validateToken(String token) {
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e){
+            return false;
+        }
+    }
 }
